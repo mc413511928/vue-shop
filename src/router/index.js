66742d978a08513691cce644c28fc8ca0ab04eef
mc413511928 <413511928@qq.com>
2020-05-12 12:01:52 +0800

    跳转到主页

--- src/router/index.js
+++ src/router/index.js
@@ -13,4 +13,17 @@ const routes = [
 const router = new VueRouter({
   routes
 })
+router.beforeEach((to, from, next) => {
+  // to去往哪个路径
+  // from来自哪个路径
+  // next同行
+  // if (to.path === '/login') return next()
+  if (to.path === '/login') return next()
+  const tokenstr = window.sessionStorage.getItem('token')
+  if (tokenstr) {
+    next()
+  } else {
+    next('/login')
+  }
+})
 export default router
