3ec2efae25ffcabea60393dc7fbb5793a8483f61
mc413511928 <413511928@qq.com>
2020-05-12 18:58:33 +0800

    主页布局

--- src/main.js
+++ src/main.js
@@ -7,12 +7,16 @@ import 'element-ui/lib/theme-chalk/index.css'
 import '../src/assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
-
+// 配置根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 拦截器
+axios.interceptors.request.use(config => {
+  // 为请求头对象，添加token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
-
 Vue.use(ElementUI)
-
 Vue.config.productionTip = false
 
 new Vue({
