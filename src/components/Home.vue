cb4cf1d1ca63db668ed57494ad19ce05a241920d
mc413511928 <413511928@qq.com>
2020-05-13 10:13:54 +0800

    完成首页功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,8 +14,7 @@
       <el-aside width="200px">
          <el-menu  background-color="#333744"
             text-color="#fff"
-            active-text-color="#409BFF"
-      >
+            active-text-color="#409BFF" unique-opened router>
       <!-- 一级菜单 -->
       <el-submenu :index="item.id+''" v-for="item in menuList" :key="item.id">
         <template slot="title">
@@ -23,7 +22,7 @@
           <span>{{item.authName}}</span>
         </template>
         <!-- 二级菜单 -->
-        <el-menu-item  :index="subItem.id+''" v-for="subItem in item.children" :key="subItem.id">
+        <el-menu-item  :index="'/'+subItem.path+''" v-for="subItem in item.children" :key="subItem.id">
           <template slot="title">
           <i class="el-icon-menu"></i>
           <span>{{subItem.authName}}</span>
@@ -31,7 +30,9 @@
         </el-submenu>
     </el-menu>
       </el-aside>
-      <el-main>Main</el-main>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
@@ -94,9 +95,15 @@ export default {
 }
 
 .el-aside{
-  background-color:#333744
+  background-color:#333744;
+  .el-menu{
+    border-right: none;
+  }
 }
 .el-main{
   background-color:#eaedf1;
+}
+.iconfont{
+  margin-right: 10px;
 }
 </style>
